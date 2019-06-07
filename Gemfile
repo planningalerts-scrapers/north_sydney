ba2c7a15a6823810397492e84dd453ca5a04f4cd
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 13:22:45 +1000

    Switch over to using gem

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 ruby "~>2.4"
 
-gem 'scraperwiki', git: 'https://github.com/openaustralia/scraperwiki-ruby.git', branch: 'morph_defaults'
-gem 'mechanize'
+gem "icon_scraper", git: "https://github.com/planningalerts-scrapers/icon_scraper.git"
+gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
 
 gem "vcr"
 gem "webmock"
--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
+require "icon_scraper"
 
 case ENV['MORPH_PERIOD']
   when 'lastmonth'
@@ -13,4 +13,4 @@ case ENV['MORPH_PERIOD']
 end
 puts "Getting data in `" + period + "`, changable via MORPH_PERIOD environment"
 
-scrape_icon_rest_xml("https://apptracking.northsydney.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=" + period + "&k=LodgementDate&o=xml")
+IconScraper.rest_xml("https://apptracking.northsydney.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=" + period + "&k=LodgementDate&o=xml")
