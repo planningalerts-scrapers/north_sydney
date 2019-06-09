6fb02bf992cb06a222b939d140b85d2530f34a3e
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 02:35:07 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "icon_scraper"
 
-IconScraper.rest_xml(
-  "https://apptracking.northsydney.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx",
-  "d=last14days&k=LodgementDate&o=xml"
-)
+IconScraper.scrape_and_save(:north_sydney)
